dc2ba66bee4e45824ca4b9924b9cb53521d70569
Andrea De Angelis <andreadeangelis84@gmail.com>
2020-04-08 13:04:28 +0200

    Set up for session 8

--- scripts/01_import.R
+++ scripts/01_import.R
@@ -1,4 +1,5 @@
-
 dat <- rio::import(here::here("data", "replication_data_final_AEST.tsv"))
 
-
+# dir.create(here::here("data_recoded"))
+save(dat, 
+     file = here::here("data_recoded", "dat_toprocess.Rdata"))
---
+++ scripts/02_preprocessing.R
@@ -0,0 +1,10 @@
+# Load data
+
+
+
+# 1. Select only the three key variables (something else?)
+
+# 2. Drop missing values
+
+# 3. Save
+
