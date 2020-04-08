3cbc07c298dc0f58af3e3f3ce11833669c1cbab4
Andrea De Angelis <andreadeangelis84@gmail.com>
2020-04-08 18:08:15 +0200

    p[reprocessing

---
+++ scripts/02_preprocessing.R
@@ -0,0 +1,17 @@
+# Load data
+library(tidyverse)
+
+# dat <- readRDS("dat_toprocess.Rdata")
+load(file = here::here("data_recoded", 'dat_toprocess.Rdata'))
+
+
+# 1. Select only the three key variables (something else?)
+dat <- as_tibble(dat)
+dat %>% 
+  select(party_ch_all_voters_t, emp_ch1_10, interpolated_expert_ch1_10) 
+
+
+# 2. Drop missing values
+
+# 3. Save
+
